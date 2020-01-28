86ac7a776f131a4d1e1f0c833a3a831cef09a782
Igor ambonatti <contatoambonatti@gmail.com>
2020-01-28 17:28:16 -0300

    Correção

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,41 +1,11 @@
-import React, { useEffect, useState } from 'react';
-
-import api from '../../services/api';
+import React from 'react';
 
 import { Container, Content } from './styles';
 
 export default function Home() {
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    async function loadTodos() {
-      const response = await api.get(
-        'https://jsonplaceholder.typicode.com/todos'
-      );
-
-      setTodos(response.data);
-    }
-    loadTodos();
-  }, []);
   return (
-    <Content>
-      <Container>
-        <input type="text" />
-        <table border="1">
-          <tr>
-            <th>userId</th>
-            <th>id</th>
-            <th>title</th>
-          </tr>
-          {todos.map(e => (
-            <tr>
-              <td>{e.userId}</td>
-              <td>{e.id}</td>
-              <td>{e.title}</td>
-            </tr>
-          ))}
-        </table>
-      </Container>
-    </Content>
+    <Container>
+      <Content />
+    </Container>
   );
 }
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,10 +1,4 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  display: block;
-  justify-content: center;
-`;
-export const Content = styled.div`
-  display: flex;
-  justify-content: center;
-`;
+export const Container = styled.div``;
+export const Content = styled.div``;
